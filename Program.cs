da4f39c825278008cd84f259efe85768d1c9d902
nicolas <Kiko2008.emilio@gmail.com>
2025-10-03 19:46:13 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("digite seu primeiro nome");
+string nome = Console.ReadLine();
+Console.WriteLine("digite seu sobrenome");
+string sobrenome =Console.ReadLine();
+Console.WriteLine( $"nome completo {nome} {sobrenome}" );
+Console.WriteLine( $"nome de catalogo: {sobrenome.ToUpper()} {nome}");
